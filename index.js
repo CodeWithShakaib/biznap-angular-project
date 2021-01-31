4200325eaf21d5d01c6e9170b80ae6dba82de53e
CodeWithShakaib <shakaib@codesorbit.com>
2021-01-31 23:46:12 +0500

    false url handling

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/biznap/dist/index.html'));
 })
 
+app.all('*', function(req, res) {
+    res.send("<h1>Bad request</h1><br><p>Please use base url, click <a href='https://biznap-admin.herokuapp.com/'>BizNap-Admin</a> and proceed</p>")
+})
+
 app.listen(port, function() {
 
     console.log("listening at http://%s:%s")
